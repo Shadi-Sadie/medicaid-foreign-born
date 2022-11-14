c210c1fbb0ece986fc52709e7f4e05553d0569d2
Shadi_Sadie <shadi.sedi@gmail.com>
2022-11-14 16:59:30 -0500

    Update GetDataTidcyCensus-00.R
    I added the missed variable TYPE, added code to format data and variables. And fixed the observed error for the variable YOEP for the US borns.

--- Codes/R/GetDataTidcyCensus-00.R
+++ Codes/R/GetDataTidcyCensus-00.R
@@ -30,7 +30,7 @@ for( i in 2009:2019)  {
                 variables = c ("REGION","AGEP", "CIT" , "ENG" , "FER","HINS1", 
                                "HINS2", "HINS3" , "HINS4","HINS5", "HINS6", "HINS7",
                                "MAR", "SCHL", "SEX", "YOEP","DIS","HICOV", "ESR", 
-                                "NATIVITY","HISP", "POVPIP", "RAC1P"),
+                                "NATIVITY","HISP", "POVPIP", "RAC1P", "TYPE"),
                 state = "all",
                 year = i, 
                 variables_filter = list(
@@ -48,9 +48,51 @@ for( i in 2009:2019)  {
 
 ACS<-rbind(ACS2009,ACS2010,ACS2011,ACS2012,ACS2013,ACS2014,ACS2015,ACS2016,ACS2017,ACS2018,ACS2019)
 
+### Change the data fromat from tbl-df to data frame 
+
+
+ACS=as.data.frame(ACS)
+
+
+#### Fixing the charachter to integer 
+
+classcol<-sapply(ACS, class)
+table(classcol)
+char_columns <- which(classcol=="character")   
+for (i in char_columns){
+        ACS[,i] <- as.numeric(ACS[,i])
+}
+
+
+
+## Upon checking all the variable and their value I noticed that  YOEP (Year of entry) for people borned in US is bottem coded instead of being NA , 
+## thus I fix this mannaualy based on the data dictionary for each year 
+
+# I can do it either in this way  
+
+ACS$YOEP<-ifelse(ACS$YEAR==2009 & ACS$YOEP==1918,NA,ACS$YOEP) #
+ACS$YOEP<-ifelse(ACS$YEAR==2010 & ACS$YOEP==1918,NA,ACS$YOEP) # 
+ACS$YOEP<-ifelse(ACS$YEAR==2011 & ACS$YOEP==1918,NA,ACS$YOEP) # 
+ACS$YOEP<-ifelse(ACS$YEAR==2012 & ACS$YOEP==1920,NA,ACS$YOEP) # 
+ACS$YOEP<-ifelse(ACS$YEAR==2013 & ACS$YOEP==1920,NA,ACS$YOEP) # 
+ACS$YOEP<-ifelse(ACS$YEAR==2014 & ACS$YOEP==1920,NA,ACS$YOEP) # 
+ACS$YOEP<-ifelse(ACS$YEAR==2015 & ACS$YOEP==1920,NA,ACS$YOEP) # 
+ACS$YOEP<-ifelse(ACS$YEAR==2016 & ACS$YOEP==1920,NA,ACS$YOEP) # 
+ACS$YOEP<-ifelse(ACS$YEAR==2017 & ACS$YOEP==1924,NA,ACS$YOEP) # 
+ACS$YOEP<-ifelse(ACS$YEAR==2018 & ACS$YOEP==1928,NA,ACS$YOEP) # 
+ACS$YOEP<-ifelse(ACS$YEAR==2019 & ACS$YOEP==1928,NA,ACS$YOEP) # 
+
+
+# or 
+
+        # ACS$YOEP<-ifelse(ACS$CIT==1,NA,ACS$YOEP) # 
+
+
 ### Export this dataset as RAWACS for the future use 
 
-write.csv(df, file = "RAWACS.csv", row.names = FALSE)
+
+
+write.csv(Data, file = "RAWACS.csv", row.names = FALSE)
 
 
 
