6e35d63ba1673c8d9651f7696981a276afd11628
Shadi Sadie <shadi.sedi@gmail.com>
2023-08-07 10:52:18 -0400

    update did regression with prelim result

--- codes/R/did-regressions.R
+++ codes/R/did-regressions.R
@@ -1,207 +1,187 @@
-## Difference-in-Differences Model Results
 
-#include region-by-year fixed effects contains state-level time varying
-#demographic and other policy controls. state-specific trend
-
-### Weighted OLS
-
-#### Table 1: The effect of the Medicaid expansion on uninsured rate.(OLS Weighted) for all low-income adult aged 26-65
+# Title: ## Difference-in-Differences Model Results
+# Author: Shadi Seyedi
+# Date: July 5, 2023
+# Description:  This code section Includes codes for the DID analysis it include both ols and logit results with two tables
 
 
-reg1 = feols(UNINS ~ treat | ST + YEAR , cluster = ~ST, weights = ~PWGTP, data = Data) # state and year fixed effect
-reg2 = feols(UNINS ~ treat | ST + YEAR + REGION^YEAR,cluster = ~ST, weights = ~PWGTP, data = Data) # State & Year & Region by year
-formulareg3 <- as.formula(paste("UNINS ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA",
-                                
-                                "| ST + YEAR + REGION^YEAR"))
-reg3 <- feols(formulareg3, cluster = ~ST, weights = ~PWGTP, data = Data)
 
-formulareg4 <- as.formula(paste("UNINS ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA",
-                                "| ST + YEAR + REGION^YEAR + ST[YEAR]"))
+####################
+## Required Packages
+####################
 
-reg4 <- feols(formulareg4, cluster = ~ST, weights = ~PWGTP, data = Data)
+library(fixest)
 
 
-etable(reg1, reg2, reg3,reg4, headers = c("(1)", "(2)", "(3)", "(4)"),
+####################
+## Data Preparation 
+####################
+
+Data$ForeginBorn <- ifelse(Data$NATIVITY == "Foregin-born", 1, 0)
+# Make sure data are factor
+data<-Data
+factor_columns <- sapply(data, is.factor)
+data[factor_columns] <- lapply(data[factor_columns], as.character)
+
+data <- replace(data, is.na(data), 99999999)
+
+data[factor_columns] <- lapply(data[factor_columns], as.factor)
+
+
+################################################################################
+#### Table 1
+#### The effect of the Medicaid expansion on uninsured rate.
+#### (OLS Weighted) for all low-income adult aged 26-65
+################################################################################
+
+# this is where I found good info on how to rotate the table
+#  # # #"https://stackoverflow.com/questions/25849814/rstudio-rmarkdown-both-portrait-and-landscape-layout-in-a-single-pdf
+
+
+## OLS Regressions Result
+
+reg1 = feols(UNINS ~treat*ForeginBorn | ST + YEAR , vcov = "hetero", weights = ~PWGTP, data = data) # state and year fixed effect
+reg2 = feols(UNINS ~treat*ForeginBorn+.[adjs]  | ST + YEAR+ sw0(REGION^YEAR) , vcov = "hetero", weights = ~PWGTP, data = data) # state and year fixed effect
+reg3 = feols(UNINS ~treat*ForeginBorn+.[adjs]  | ST + YEAR + ST[YEAR], vcov = "hetero", weights = ~PWGTP, data = data) # state and year fixed effect
+
+reg4 = feols(UNINS ~ treat*ForeginBorn + ForeginBorn*.[controls]+ ForeginBorn*.[foriegn]| ST + YEAR +sw0(REGION^YEAR), vcov = "hetero", weights = ~PWGTP, data = data) # state and year fixed effect
+
+reg5 = feols(UNINS ~ treat*ForeginBorn + ForeginBorn*.[controls]+ ForeginBorn*.[foriegn] | ST + YEAR + ST[YEAR], vcov = "hetero", weights = ~PWGTP, data = data) # state and year fixed effect
+
+etable(reg1, reg2, reg3,reg4,reg5, 
        vcov = "twoway", markdown=TRUE)
 
 
-#### Table 2: The effect of the Medicaid expansion on medicaid take-up.(OLS Weighted) for all low-income adult aged 26-65
+## Logit Regressions Result
+reg6 = feglm(UNINS ~treat*ForeginBorn | ST + YEAR , vcov = "hetero", weights = ~PWGTP, data = data, family = 'logit') # state and year fixed effect
+reg7 = feglm(UNINS ~treat*ForeginBorn+.[adjs]  | ST + YEAR+ sw0(REGION^YEAR) , vcov = "hetero", weights = ~PWGTP, data = data, family = 'logit') # state and year fixed effect
+reg8 = feglm(UNINS ~treat*ForeginBorn+.[adjs]  | ST + YEAR + ST[YEAR], vcov = "hetero", weights = ~PWGTP, data = data, family = 'logit') # state and year fixed effect
+
+reg9 = feglm(UNINS ~ treat*ForeginBorn + ForeginBorn*.[controls]+ ForeginBorn*.[foriegn]| ST + YEAR +sw0(REGION^YEAR), vcov = "hetero", weights = ~PWGTP, data = data, family = 'logit') # state and year fixed effect
+
+reg0 = feglm(UNINS ~ treat*ForeginBorn + ForeginBorn*.[controls]+ ForeginBorn*.[foriegn] | ST + YEAR + ST[YEAR], vcov = "hetero", weights = ~PWGTP, data = data, family = 'logit') # state and year fixed effect
 
 
-reg1 = feols(HINS4 ~ treat | ST + YEAR , cluster = ~ST, weights = ~PWGTP, data = Data) # state and year fixed effect
-reg2 = feols(HINS4 ~ treat | ST + YEAR + REGION^YEAR,cluster = ~ST, weights = ~PWGTP, data = Data) # State & Year & Region by year
-formulareg3 <- as.formula(paste("HINS4 ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA",
-                                
-                                "| ST + YEAR + REGION^YEAR"))
-reg3 <- feols(formulareg3, cluster = ~ST, weights = ~PWGTP, data = Data)
+etable(reg6, reg7, reg3,reg8,reg9,reg0, 
+       vcov = "twoway", markdown=TRUE)
 
-formulareg4 <- as.formula(paste("HINS4 ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA",
-                                "| ST + YEAR + REGION^YEAR + ST[YEAR]"))
 
-reg4 <- feols(formulareg4, cluster = ~ST, weights = ~PWGTP, data = Data)
+## Full table result
 
-etable(reg1, reg2, reg3,reg4,
+tab<-
+        etable(list(reg1,reg2,reg3, reg4,reg5,reg6,reg7,reg8, reg9,reg0), 
+               title = "The Effect of Medicaid Expansion on Uninsured Rate (Difference-in-Differences Estimation)",
+               headers = list("^:_:"= .("FE OLS" = 7, "FE LOGIT"=7)),
+               # extralines=list("^^Uninsured"=.(" ")) ,
+               group = list("-^Controls"=c(controls,foriegn)),
+               digits = 3,
+               adjustbox= 1.5,
+               tex= TRUE,
+               se.row = TRUE, 
+               depvar= FALSE,
+               family = FALSE,
+               arraystretch = 1,
+               convergence = T,
+               fitstat = ~ n +r2 +ar2 + pr2+  aic + bic ,
+               # notes = c("note 1", "source 1"),
+               style.tex = style.tex(
+                       main = "base",
+                       fixef.suffix = " FE",
+                       fixef.title = "\\midrule",
+                       stats.title = "\\midrule",
+                       # tabular = "*",
+                       model.title = "",
+                       #  model.format = "[i]",
+                       # fontsize= 'normalsize', 
+                       tpt=T,
+                       depvar.title= " ",
+                       var.title = "\\midrule",
+                       slopes.title = "",
+                       slopes.format = "State-Specific Linear Time Trends",
+                       yesNo = "yes"
+               ) ,
+               dict=c(ST = "State" , YEAR = "Year", UNINS="Uninsured Rate", treat='Medicaid Expansion',
+                      ForeginBorn= 'Foreign-Born',UnempR= "State Unemployment Rate", ADA="State Political Ideology", REGION="Region"
+               )
+        )
+
+print(tab)
+
+
+################################################################################
+#### Table 2
+#### The effect of the Medicaid expansion on medicaid take-up.
+#### (OLS Weighted) for all low-income adult aged 26-65
+################################################################################
+
+## OLS Regression 
+
+reg1 = feols(HINS4 ~treat*ForeginBorn | ST + YEAR , vcov = "hetero", weights = ~PWGTP, data = data) # state and year fixed effect
+reg2 = feols(HINS4 ~treat*ForeginBorn+.[adjs]  | ST + YEAR+ sw0(REGION^YEAR) , vcov = "hetero", weights = ~PWGTP, data = data) # state and year fixed effect
+reg3 = feols(HINS4 ~treat*ForeginBorn+.[adjs]  | ST + YEAR + ST[YEAR], vcov = "hetero", weights = ~PWGTP, data = data) # state and year fixed effect
+
+reg4 = feols(HINS4 ~ treat*ForeginBorn + ForeginBorn*.[controls]+ ForeginBorn*.[foriegn]| ST + YEAR +sw0(REGION^YEAR), vcov = "hetero", weights = ~PWGTP, data = data) # state and year fixed effect
+
+reg5 = feols(HINS4 ~ treat*ForeginBorn + ForeginBorn*.[controls]+ ForeginBorn*.[foriegn] | ST + YEAR + ST[YEAR], vcov = "hetero", weights = ~PWGTP, data = data) # state and year fixed effect
+etable(reg1, reg2, reg3,reg4, reg5,
        vcov = "twoway", headers = c("(1)", "(2)", "(3)", "(4)"))
 
 
-
-#### Table 3: The effect of the Medicaid expansion on uninsured.(OLS Weighted) for all US-Born adult with low-income adult and aged 26-65
-
-
-reg1 = feols(UNINS ~ treat | ST + YEAR , cluster = ~ST, weights = ~PWGTP, data = NATV) # state and year fixed effect
-reg2 = feols(UNINS ~ treat | ST + YEAR + REGION^YEAR,cluster = ~ST, weights = ~PWGTP, data = NATV) # State & Year & Region by year
-formulareg3 <- as.formula(paste("UNINS ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA",
-                                
-                                "| ST + YEAR + REGION^YEAR"))
-reg3 <- feols(formulareg3, cluster = ~ST, weights = ~PWGTP, data = NATV)
-
-formulareg4 <- as.formula(paste("UNINS ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA",
-                                "| ST + YEAR + REGION^YEAR + ST[YEAR]"))
-
-reg4 <- feols(formulareg4, cluster = ~ST, weights = ~PWGTP, data = NATV)
-
-etable(reg1, reg2, reg3,reg4,
-       vcov = "twoway", headers = c("(1)", "(2)", "(3)", "(4)"))
+## Logit regressions
 
 
-#### Table 4: The effect of the Medicaid expansion on medicaid take-up.(OLS Weighted) for US-Born adult low-income adult aged 26-65
+reg6 = feglm(HINS4 ~treat*ForeginBorn | ST + YEAR , vcov = "hetero", weights = ~PWGTP, data = data, family = 'logit') # state and year fixed effect
+reg7 = feglm(HINS4 ~treat*ForeginBorn+.[adjs]  | ST + YEAR+ sw0(REGION^YEAR) , vcov = "hetero", weights = ~PWGTP, data = data, family = 'logit') # state and year fixed effect
+reg8 = feglm(HINS4 ~treat*ForeginBorn+.[adjs]  | ST + YEAR + ST[YEAR],vcov = "hetero", weights = ~PWGTP, data = data, family = 'logit') # state and year fixed effect
+
+reg9 = feglm(HINS4 ~ treat*ForeginBorn + ForeginBorn*.[controls]+ ForeginBorn*.[foriegn]| ST + YEAR +sw0(REGION^YEAR), cluster = ~ST, weights = ~PWGTP, data = data, family = 'logit') # state and year fixed effect
+
+reg0 = feglm(HINS4 ~ treat*ForeginBorn + ForeginBorn*.[controls]+ ForeginBorn*.[foriegn] | ST + YEAR + ST[YEAR], vcov = "hetero", weights = ~PWGTP, data = data, family = 'logit') # state and year fixed effect
+
+etable(reg6, reg7, reg3,reg8,reg9,reg0, 
+       vcov = "twoway", markdown=TRUE)
 
 
-reg1 = feols(HINS4 ~ treat | ST + YEAR , cluster = ~ST, weights = ~PWGTP, data = NATV) # state and year fixed effect
-reg2 = feols(HINS4 ~ treat | ST + YEAR + REGION^YEAR,cluster = ~ST, weights = ~PWGTP, data = NATV) # State & Year & Region by year
-formulareg3 <- as.formula(paste("HINS4 ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA",
-                                "| ST + YEAR + REGION^YEAR"))
-reg3 <- feols(formulareg3, cluster = ~ST, weights = ~PWGTP, data = NATV)
+#### Full table Result
 
-formulareg4 <- as.formula(paste("HINS4 ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA",
-                                "| ST + YEAR + REGION^YEAR + ST[YEAR]"))
+table2<-
+        etable(list(reg1,reg2,reg3, reg4,reg5,reg6,reg7,reg8, reg9,reg0), 
+               title = "The Effect of Medicaid Expansion on Medicaid Covergare Rate (Difference-in-Differences Estimation)",
+               headers = list("^:_:"= .("FE OLS" = 7, "FE LOGIT"=7)),
+               
+               # extralines=list("^^Uninsured"=.(" ")) ,
+               group = list("-^Controls"=c(controls,foriegn)),
+               digits = 3,
+               vcov = "hetero",
+               adjustbox= 1.5,
+               tex= TRUE,
+               depvar= FALSE,
+               family = FALSE,
+               arraystretch = 1,
+               convergence = T,
+               se.row = TRUE, 
+               
+               fitstat = ~ n +r2 +ar2 + pr2+  aic + bic ,
+               # notes = c("note 1", "source 1"),
+               style.tex = style.tex(
+                       main = "base",
+                       fixef.suffix = " FE",
+                       fixef.title = "\\midrule",
+                       stats.title = "\\midrule",
+                       # tabular = "*",
+                       model.title = "",
+                       #  model.format = "[i]",
+                       fontsize= 'large', 
+                       tpt=T,
+                       depvar.title= " ",
+                       var.title = "\\midrule",
+                       slopes.title = "",
+                       slopes.format = "State-Specific Linear Time Trends",
+                       yesNo = "yes"
+               ) ,
+               dict=c(ST = "State" , YEAR = "Year", UNINS="Uninsured Rate", treat='Medicaid Expansion',
+                      ForeginBorn= 'Foreign-Born',UnempR= "State Unemployment Rate", ADA="State Political Ideology", REGION="Region"
+               )
+        )
 
-reg4 <- feols(formulareg4, cluster = ~ST, weights = ~PWGTP, data = NATV)
-
-etable(reg1, reg2, reg3,reg4,
-       vcov = "twoway", headers = c("(1)", "(2)", "(3)", "(4)"))
-
-
-#### Table 5: The effect of the Medicaid expansion on Uninsured rate.(OLS Weighted) for Foreign-Born low-income adult aged 26-65
-
-
-reg1 = feols(UNINS ~ treat | ST + YEAR , cluster = ~ST, weights = ~PWGTP, data = Forgn) # state and year fixed effect
-reg2 = feols(UNINS ~ treat | ST + YEAR + REGION^YEAR,cluster = ~ST, weights = ~PWGTP, data = Forgn) # State & Year & Region by year
-formulareg3 <- as.formula(paste("UNINS ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA+ IPCINDX",
-                                "+ENG +YLIU+ GEOR",
-                                "| ST + YEAR + REGION^YEAR"))
-reg3 <- feols(formulareg3, cluster = ~ST, weights = ~PWGTP, data = Forgn)
-
-formulareg4 <- as.formula(paste("UNINS ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA+ IPCINDX",
-                                "+ENG +YLIU + GEOR",
-                                "| ST + YEAR + REGION^YEAR + ST[YEAR]"))
-
-reg4 <- feols(formulareg4, cluster = ~ST, weights = ~PWGTP, data = Forgn)
-
-etable(reg1, reg2, reg3,reg4,
-       vcov = "twoway", headers = c("(1)", "(2)", "(3)", "(4)"))
-
-
-#### Table 6: The effect of the Medicaid expansion on Medicaid take-up.(OLS Weighted) for Foreign-Born low-income adult aged 26-65
-
-
-reg1 = feols(HINS4 ~ treat | ST + YEAR , cluster = ~ST, weights = ~PWGTP, data = Forgn) # state and year fixed effect
-reg2 = feols(HINS4 ~ treat | ST + YEAR + REGION^YEAR,cluster = ~ST, weights = ~PWGTP, data = Forgn) # State & Year & Region by year
-formulareg3 <- as.formula(paste("HINS4 ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA+ IPCINDX",
-                                "+ENG +YLIU+ GEOR",
-                                "| ST + YEAR + REGION^YEAR"))
-reg3 <- feols(formulareg3, cluster = ~ST, weights = ~PWGTP, data = Forgn)
-
-formulareg4 <- as.formula(paste("HINS4 ~  treat +",                                  # all+ time-varying controls
-                                paste(controls, collapse = " + ") ,
-                                "+UnempR+ ADA+ IPCINDX",
-                                "+ENG +YLIU + GEOR",
-                                "| ST + YEAR + REGION^YEAR + ST[YEAR]"))
-
-reg4 <- feols(formulareg4, cluster = ~ST, weights = ~PWGTP, data = Forgn)
-
-etable(reg1, reg2, reg3,reg4,
-       vcov = "twoway", headers = c("(1)", "(2)", "(3)", "(4)"))
-
-
-
-### Generalized linear model (Logit)
-
-#### Table 7: The effect of the Medicaid expansion on uninsured rate.(OLS Weighted) for all low-income adult aged 26-65
-
-
-reg1 = feglm(UNINS ~ treat | ST + YEAR + sw0(REGION^YEAR), cluster = ~ST, weights = ~PWGTP, data = Data, family = 'logit') #
-reg2 = feglm(UNINS ~ treat +.[controls]+ .[state] | ST + YEAR + REGION^YEAR+ sw0(ST[YEAR]), cluster = ~ST, weights = ~PWGTP, data = Data, family = 'logit') #
-
-
-etable(reg1,reg2)
-
-
-#### Table 8: The effect of the Medicaid expansion on medicaid take-up.(LOGIT) for all low-income adult aged 26-65
-
-
-reg1 = feglm(HINS4 ~ treat | ST + YEAR + sw0(REGION^YEAR), cluster = ~ST, weights = ~PWGTP, data = Data, family = 'logit') #
-reg2 = feglm(HINS4 ~ treat +.[controls]+ .[state]| ST + YEAR + REGION^YEAR+ sw0(ST[YEAR]), cluster = ~ST, weights = ~PWGTP, data = Data, family = 'logit') #
-
-etable(reg1,reg2)
-
-
-
-#### Table 9: The effect of the Medicaid expansion on uninsured.(LOGIT) for all US-Born adult with low-income adult and aged 26-65
-
-
-reg1 = feglm(UNINS ~ treat | ST + YEAR + sw0(REGION^YEAR), cluster = ~ST, weights = ~PWGTP, data = NATV, family = 'logit') #
-reg2 = feglm(UNINS ~ treat +.[controls]+ .[state]| ST + YEAR + REGION^YEAR+ sw0(ST[YEAR]), cluster = ~ST, weights = ~PWGTP, data = NATV, family = 'logit') #
-etable(reg1,reg2)
-
-
-#### Table 10: The effect of the Medicaid expansion on medicaid take-up.(OLS Weighted) for US-Born adult low-income adult aged 26-65
-
-
-reg1 = feglm(HINS4 ~ treat | ST + YEAR + sw0(REGION^YEAR), cluster = ~ST, weights = ~PWGTP, data = NATV, family = 'logit') #
-reg2 = feglm(HINS4 ~ treat +.[controls]+ .[state]| ST + YEAR + REGION^YEAR+ sw0(ST[YEAR]), cluster = ~ST, weights = ~PWGTP, data = NATV, family = 'logit') #
-etable(reg1,reg2)
-
-
-#### Table 11: The effect of the Medicaid expansion on Uninsured rate for Foreign-Born low-income adult aged 26-65
-
-reg1 = feglm(UNINS ~ treat | ST + YEAR + sw0(REGION^YEAR), cluster = ~ST, weights = ~PWGTP, data = Forgn, family = 'logit') #
-reg2 = feglm(UNINS ~ treat +.[controls]+ .[foriegn]+.[statefor]| ST + YEAR + REGION^YEAR+ sw0(ST[YEAR]), cluster = ~ST, weights = ~PWGTP, data = Forgn, family = 'logit') #
-etable(reg1,reg2)
-
-
-#### Table 12: The effect of the Medicaid expansion on Medicaid take-up for Foreign-Born low-income adult aged 26-65
-
-reg1 = feglm(HINS4 ~ treat | ST + YEAR + sw0(REGION^YEAR), cluster = ~ST, weights = ~PWGTP, data = Forgn, family = 'logit') #
-reg2 = feglm(HINS4 ~ treat +.[controls]+ .[foriegn]+.[statefor]| ST + YEAR + REGION^YEAR+ sw0(ST[YEAR]), cluster = ~ST, weights = ~PWGTP, data = Forgn, family = 'logit') #
-etable(reg1,reg2)
-
-\begin{table}[htb]
-\caption{Descriptive Statistics and Correlations for Variables}
-tab1
-tab2
-[1]\ Footnote 1: Variable~1 is calculated as\ldots \endgraf
-[2]\ Correlations are Pearson.
-\end{table}
+print(table2)
