37a859c552d65f54170c8bbfe394a0ba7e259262
Shadi Sadie <shadi.sedi@gmail.com>
2025-10-16 18:41:52 -0400

    removed the cleaning from here and moved to the cleaning

--- codes/R/00-get-data-tidcycensus.R
+++ codes/R/00-get-data-tidcycensus.R
@@ -2,19 +2,28 @@
 ##############################################################################################################
 ### This program pulls the data for non-elderly adults with low income from ACS API and merge them in a 
 ### single data set called RAWACS needs further cleaning to be used for the Medicaid and Immigrant Analysis
-### It was updated on Nov 11, 2022. 
+### It was updated on Oct 16, 2025. 
 ##############################################################################################################
 
-### Instaling required packages
+### Installing required packages
 
-#install.packages("tidycensus")
-#install.packages("tigris")
 #install.packages('Rcpp') 
+#install.packages("tidycensus")
 
 ### Loading Required Packages 
 
 library(tidycensus)
 library(tidyverse)
+
+
+wd <- list()
+# commonly used paths in my working directory  #######NOTE: UPDATE this section for the reproducibility## #################
+wd$data   <- "/home/shadi/Projects/GitHub/medicaid-foreign-born/data/"
+wd$output <- "/home/shadi/Projects/GitHub/medicaid-foreign-born/output/"
+wd$texts <- "/home/shadi/Projects/GitHub/medicaid-foreign-born/text/"
+wd$codes <- "/home/shadi/Projects/GitHub/medicaid-foreign-born/codes/R/"
+
+
 
 ### Uploading the Key to access to the ACS's API
 
@@ -22,83 +31,53 @@ census_api_key("94b2e4e0fbd4ea5b12065dabc6c8b7292a615248", install = TRUE)
 
 
 ### Creating a for loop to dl all the annual dataset at once  ##
+# Note: POVPIP and AGEP are filtered at source for speed.
+
 
 ldf <- list() # creates a list
 
-for( i in 2011:2019)  {                            
+for ( i in 2011:2019 )  {
+        
+        vars <- c("REGION","AGEP","CIT","ENG","FER",
+                  "HINS1","HINS2","HINS3","HINS4","HINS5","HINS6","HINS7",
+                  "MAR","SCHL","SEX","YOEP","DIS","HICOV","ESR",
+                  "NATIVITY","HISP","POVPIP","RAC1P","TYPE","LANP","LANX","POBP","PUMA",
+                  "COW","MIL","OCCP","SCH","HHT","RELP")
+        
+        if (i == 2019) vars[length(vars)] <- "RELSHIPP"  # <- only 2019 uses RELSHIPP
+        
         ldf[[i]] <- get_pums(
-                variables = c ("REGION","AGEP", "CIT" , "ENG" , "FER","HINS1", 
-                               "HINS2", "HINS3" , "HINS4","HINS5", "HINS6", "HINS7",
-                               "MAR", "SCHL", "SEX", "YOEP","DIS","HICOV", "ESR", 
-                                "NATIVITY","HISP", "POVPIP", "RAC1P", "TYPE","LANP","LANX","POBP","PUMA",
-                               "COW","MIL","OCCP","SCH","HHT","RELP"
-                               ),
+                variables = vars,
                 state = "all",
-                year = i, 
-                variables_filter = list(
-                        AGEP = 19:64, POVPIP = 000:138 ## Filtering age and income #
-                ),
+                year = i,
+                variables_filter = list(AGEP = 19:64, POVPIP = 000:138),
                 survey = "acs1",
-                rep_weights = "person", 
-                recode = FALSE,
+                rep_weights = "person",
+                recode = FALSE
         )
-        ldf[[i]]$YEAR<-i ## Creat year and assigne value ##
-        assign(paste0("ACS",i),ldf[[i]] )
+        
+        ldf[[i]]$YEAR <- i
+        assign(paste0("ACS", i), ldf[[i]])
 }
 
-## Variable name for relation has deferent name should be changed
-names(ACS2019)[names(ACS2019) == "RELSHIPP"] <- "RELP"
-### Bind all the anndata:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAABWzo5XAAAAbElEQVR4Xs2RQQrAMAgEfZgf7W9LAguybljJpR3wEse5JOL3ZObDb4x1loDhHbBOFU6i2Ddnw2KNiXcdAXygJlwE8OFVBHDgKrLgSInN4WMe9iXiqIVsTMjH7z/GhNTEibOxQswcYIWYOR/zAjBJfiXh3jZ6AAAAAElFTkSuQmCCual date in a single data set 
 
+## The relationship variable has a different name in 2019; rename it to RELP.
+names(ACS2019)[names(ACS2019) == "RELSHIPP"] <- "RELP"
+
+
+
+
+## bind all the data in one dataset 
 ACS<-rbind(ACS2011,ACS2012,ACS2013,ACS2014,ACS2015,ACS2016,ACS2017,ACS2018,ACS2019)
 
-### Change the data fromat from tbl-df to data frame 
-
+### Change the data format from tbl-df to data frame 
 
 ACS=as.data.frame(ACS)
 
 
-#### Fixing the charachter to factro 
+#### 
 
-classcol<-sapply(ACS, class)
-table(classcol)
-char_columns <- which(classcol=="character")   
-for (i in char_columns) {
-        if (colnames(ACS)[i] != "SERIALNO") {
-                ACS[, i] <- as.numeric(ACS[, i])
-        }
-}
-## don't worry warninig is because b in data are changing to NA
+write.csv(ACS, file = paste0(wd$data,"RAWACS-2025.csv"), row.names = FALSE)
 
 
-## Upon checking all the variable and their value I noticed that  YOEP (Year of entry) for people borned in US is bottem coded instead of being NA , 
-## thus I fix this mannaualy based on the data dictionary for each year 
-
-# I can do it either in this way  
-
-#ACS$YOEP<-ifelse(ACS$YEAR==2009 & ACS$YOEP==1918,NA,ACS$YOEP) #
-#ACS$YOEP<-ifelse(ACS$YEAR==2010 & ACS$YOEP==1918,NA,ACS$YOEP) # 
-ACS$YOEP<-ifelse(ACS$YEAR==2011 & ACS$YOEP==1918,NA,ACS$YOEP) # 
-ACS$YOEP<-ifelse(ACS$YEAR==2012 & ACS$YOEP==1920,NA,ACS$YOEP) # 
-ACS$YOEP<-ifelse(ACS$YEAR==2013 & ACS$YOEP==1920,NA,ACS$YOEP) # 
-ACS$YOEP<-ifelse(ACS$YEAR==2014 & ACS$YOEP==1920,NA,ACS$YOEP) # 
-ACS$YOEP<-ifelse(ACS$YEAR==2015 & ACS$YOEP==1920,NA,ACS$YOEP) # 
-ACS$YOEP<-ifelse(ACS$YEAR==2016 & ACS$YOEP==1920,NA,ACS$YOEP) # 
-ACS$YOEP<-ifelse(ACS$YEAR==2017 & ACS$YOEP==1924,NA,ACS$YOEP) # 
-ACS$YOEP<-ifelse(ACS$YEAR==2018 & ACS$YOEP==1928,NA,ACS$YOEP) # 
-ACS$YOEP<-ifelse(ACS$YEAR==2019 & ACS$YOEP==1928,NA,ACS$YOEP) # 
-
-
-# or 
-
-        # ACS$YOEP<-ifelse(ACS$CIT==1,NA,ACS$YOEP) # 
-
-
-### Export this dataset as RAWACS for the future use 
-
-
-
-write.csv(Data, file = paste0(wd$data,"RAWACS.csv"), row.names = FALSE)
-
-
-save(Data, file =paste0(wd$data,"RAWACS.RData"))
+save(ACS, file =paste0(wd$data,"RAWACS-2025.RData"))
